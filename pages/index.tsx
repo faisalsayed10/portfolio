94afb8bf3c4c3da243b9146479d648c18d944ef6
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-14 13:50:18 +0530

    Animated Cursor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,27 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { Navbar } from "@components/Navbar";
+import dynamic from "next/dynamic";
+
+const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-
-    </div>
-  )
+    <>
+      <AnimatedCursor
+        // @ts-ignore
+        innerSize={10}
+        outerSize={8}
+        color="0, 216, 214"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={5}
+      />
+      <Head>
+        <title>Faisal Sayed</title>
+      </Head>
+      <Navbar />
+    </>
+  );
 }

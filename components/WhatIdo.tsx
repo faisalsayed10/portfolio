4a2ba6cd76b08ea5ddf96990346b6bc0fdec0d16
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-15 17:04:21 +0530

    WhatIDo Component

---
+++ components/WhatIdo.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const WhatIdo: React.FC = () => {
+  return (
+    <>
+      <div className="">
+        <h3 className="flex items-center text-blue-800 my-6 text-xl font-medium antialiased">
+          <div className="bar bg-blue-800 mr-3"></div>What I Do
+        </h3>
+        <h2 className="text-4xl mb-16 font-semibold antialiased">
+          <p>I enjoy creating products,</p>
+          <p>which are fun and delightful.</p>
+        </h2>
+        <h1 className="text-8xl font-semibold antialiased">
+          <p>Think.&nbsp;Make.</p>
+          <p>Ship.</p>
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default WhatIdo;

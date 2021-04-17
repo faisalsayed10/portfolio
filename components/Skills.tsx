679887c8b94e3aa114ed22a61faaeb3db8eb38b4
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-17 13:03:37 +0530

    Skills component and icons

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,18 +1,70 @@
 import React from "react";
+import {
+  faGitAlt,
+  faHtml5,
+  faJs,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { InlineIcon } from "@iconify/react";
+import firebase from "@iconify/icons-cib/firebase";
+import graphql from "@iconify/icons-cib/graphql";
+import mongodb from "@iconify/icons-cib/mongodb";
+import sql from "@iconify/icons-cib/postgresql";
+import express from "@iconify/icons-simple-icons/express";
+import prisma from "@iconify/icons-simple-icons/prisma";
 
 interface Props {}
 
 const Skills: React.FC<Props> = (props) => {
   return (
     <>
-      <div className="flex items-start ml-40 mb-20 justify-center flex-col">
+      <div className="ml-40 mb-20">
         <h3 className="flex items-center text-blue-800 mb-12 text-xl font-medium antialiased">
           <div className="bar bg-blue-800 mr-3"></div>My Skills
         </h3>
-        <h2 className="text-4xl mb-32 font-semibold antialiased">
-          <p>I enjoy creating products,</p>
-          <p>which are fun and delightful.</p>
-        </h2>
+        <div className="grid grid-cols-1 gap-10 md:grid-cols-3 text-xl font-medium antialiased">
+          <p>
+            <span className="text-blue-800">○</span>{" "}
+            <FontAwesomeIcon icon={faHtml5} /> HTML &amp; CSS
+          </p>
+          <p>
+            <span className="text-blue-800">○</span>{" "}
+            <FontAwesomeIcon icon={faJs} /> JavaScript / TypeScript
+          </p>
+          <p>
+            <span className="text-blue-800">○</span>{" "}
+            <FontAwesomeIcon icon={faReact} /> React / Next.js
+          </p>
+          <p>
+            <span className="text-blue-800">○</span>{" "}
+            <FontAwesomeIcon icon={faGitAlt} /> Git
+          </p>
+          <p>
+            <span className="text-blue-800">○</span>{" "}
+            <InlineIcon className="inline" icon={express} /> Express
+          </p>
+          <p>
+            <span className="text-blue-800">○</span>{" "}
+            <InlineIcon className="inline" icon={graphql} /> GraphQL
+          </p>
+          <p>
+            <span className="text-blue-800">○</span>{" "}
+            <InlineIcon className="inline" icon={firebase} /> Firebase
+          </p>
+          <p>
+            <span className="text-blue-800">○</span>{" "}
+            <InlineIcon className="inline" icon={mongodb} /> MongoDB
+          </p>
+          <p>
+            <span className="text-blue-800">○</span>{" "}
+            <InlineIcon className="inline" icon={sql} /> SQL
+          </p>
+          <p>
+            <span className="text-blue-800">○</span>{" "}
+            <InlineIcon className="inline" icon={prisma} /> Prisma
+          </p>
+        </div>
       </div>
     </>
   );

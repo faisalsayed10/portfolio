fc85a2eaca001d0ee84323061f52b3d2b21525b7
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-17 16:06:59 +0530

    mock project data

---
+++ util/mockData.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    project_image: "/gummy-coding.png",
+    title: "This is Proj",
+    desc: "DESCRIPTIONFAINFJEANFNAENFJNAE",
+    more_url: "https://example.com",
+  },
+  {
+    project_image: "/gummy-coding.png",
+    title: "This is Proj",
+    desc: "DESCRIPTIONFAINFJEANFNAENFJNAE",
+    more_url: "https://example.com",
+  },
+  {
+    project_image: "/gummy-coding.png",
+    title: "This is Proj",
+    desc: "DESCRIPTIONFAINFJEANFNAENFJNAE",
+    more_url: "https://example.com",
+  },
+  {
+    project_image: "/gummy-coding.png",
+    title: "This is Proj",
+    desc: "DESCRIPTIONFAINFJEANFNAENFJNAE",
+    more_url: "https://example.com",
+  },
+];

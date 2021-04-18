a4af4aee11fc4f1fc7f217c37288fa695517331c
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-18 16:05:56 +0530

    tweek projectCard css

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -12,10 +12,10 @@ const ProjectCard: React.FC<Props> = ({
   more_url,
 }) => {
   return (
-    <div className="flex flex-col w-96 items-center justify-center relative rounded-3xl hover:shadow-2xl hover-trigger py-12">
-      <img src={project_image} className="hover-image w-40 h-40" />
-      <h1 className="pb-4 text-2xl font-semibold">{title}</h1>
-      <p className="pb-12 text-md">{desc}</p>
+    <div className="flex flex-col card w-96 items-center justify-evenly relative rounded-3xl hover:shadow-2xl hover-trigger px-8 my-8 mx-4">
+      <img src={project_image} className="hover-image w-auto h-36 rounded-lg" />
+      <h1 className="text-2xl font-semibold">{title}</h1>
+      <p className="text-md">{desc}</p>
       <a href={more_url}>
         <button className="rounded-xl bg-blue-900 hover-target text-white px-6 py-3">
           Learn More

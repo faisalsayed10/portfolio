7b917aefca0e6290f17425a11e19795f32014ea3
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-18 18:49:22 +0530

    finish sendMail API

---
+++ components/ContactForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+  
+}
+
+const ContactForm: React.FC<Props> = (props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ContactForm
--- pages/api/sendMail.ts
+++ pages/api/sendMail.ts
@@ -1,5 +1,26 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import sgMail from "@sendgrid/mail";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  return res.status(400).json({ text: "HELLO" });
+  const { name, email, subject, message } = req.body;
+
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+  const msg = {
+    from: process.env.EMAIL_FROM,
+    to: `faisal.sayed502@gmail.com`,
+    subject: subject,
+    text: `Email: ${email} (${name}) \n Message: ${message}`,
+  };
+
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+      return res.status(500).json({ message: error.message });
+    });
+  return res.status(200).json({ message: "Email sent!" });
 };

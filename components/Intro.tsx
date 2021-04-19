e49096404a8c63006b09aa7c3837e2f8f2ce66ab
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-19 15:02:58 +0530

    responsive design - Intro

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -23,13 +23,13 @@ const Intro: React.FC = () => {
 
   return (
     <>
-      <div className="flex bg-wave bg-cover flex-col-reverse lg:flex-row items-center lg:items-end mb-20 lg:pb-60 min-h-screen justify-between">
+      <div className="flex flex-col-reverse lg:flex-row items-center mb-20 min-h-screen justify-center lg:justify-between">
         <motion.h1
           variants={container}
           ref={ref}
           initial="hidden"
           animate={controls}
-          className="text-5xl lg:text-7xl ml-40 tracking-normal leading-tight font-semibold antialiased"
+          className="text-3xl md:text-4xl lg:text-4xl xl:text-6xl lg:ml-20 xl:ml-40 text-center lg:text-left tracking-normal leading-tight font-semibold antialiased"
         >
           {items.map((item, i) => (
             <motion.p key={i} variants={introListVariant}>
@@ -41,8 +41,9 @@ const Intro: React.FC = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.3 }}
+          className="lg:-mt-40 mb-10"
         >
-          <Lottie loop play animationData={animationData} className="w-auto" />
+          <Lottie loop play animationData={animationData} className="w-auto vr" />
         </motion.div>
       </div>
     </>

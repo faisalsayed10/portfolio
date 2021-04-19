6bb304d2f8b4cb01987c260c6ed55192892ebe66
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-19 12:07:34 +0530

    footer

---
+++ components/Footer.tsx
@@ -0,0 +1,28 @@
+import { faGithubAlt } from "@fortawesome/free-brands-svg-icons";
+import { faLevelUpAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+interface Props {}
+
+const Footer: React.FC<Props> = (props) => {
+  return (
+    <>
+      <div className="flex text-white text-lg items-center justify-around pb-6">
+        <a href="https://github.com/faisalsayed10/portfolio" target="_blank">
+          <p>
+            <FontAwesomeIcon icon={faGithubAlt} className="mr-2" /> Source
+          </p>
+        </a>
+        <a href="#header">
+          <p>
+            <FontAwesomeIcon icon={faLevelUpAlt} className="mr-2" />
+            Back to top
+          </p>
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Footer;

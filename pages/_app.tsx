65348a14ced1352221ac900341ddb1285b378c85
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-20 14:42:49 +0530

    shift cursor to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import "@styles/globals.css";
 import "tailwindcss/tailwind.css";
+import dynamic from "next/dynamic";
+
+const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
+  ssr: false,
+});
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+  return (
+    <>
+      <AnimatedCursor
+        // @ts-ignore
+        innerSize={10}
+        outerSize={8}
+        color="0, 216, 214"
+        outerAlpha={0.2}
+        innerScale={0.7}
+        outerScale={5}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import { Navbar } from "@components/Navbar";
-import dynamic from "next/dynamic";
 import Intro from "@components/Intro";
 import WhatIdo from "@components/WhatIdo";
 import { useState } from "react";
@@ -11,25 +10,12 @@ import Projects from "@components/Projects";
 import ContactForm from "@components/Contact";
 import Footer from "@components/Footer";
 
-const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
-	ssr: false,
-});
-
 export default function Home() {
 	const [open, setOpen] = useState(false);
 	const [play, setPlay] = useState(false);
 
 	return (
 		<>
-			<AnimatedCursor
-				// @ts-ignore
-				innerSize={10}
-				outerSize={8}
-				color="0, 216, 214"
-				outerAlpha={0.2}
-				innerScale={0.7}
-				outerScale={5}
-			/>
 			<Head>
 				<title>Faisal Sayed</title>
 			</Head>

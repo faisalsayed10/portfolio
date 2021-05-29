1fa6d89f408f2f3c73265d8cc7fbbb029ff6169a
faisalsayed10 <faisal.sayed502@gmail.com>
2021-05-29 13:00:52 +0530

    Initialize AOS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
 import "@styles/globals.css";
 import "tailwindcss/tailwind.css";
 import dynamic from "next/dynamic";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
   ssr: false,
 });
 
 function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+      AOS.init();
+    }, []);
+
+
   return (
     <>
       <AnimatedCursor

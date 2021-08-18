05d5198bdfcec86e71f3346a8e89f27084ad9700
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-08-18 20:49:37 +0800

    Fix Typescript

--- pages/api/workshops.ts
+++ pages/api/workshops.ts
@@ -1,3 +1,5 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const data = await fetch('https://workshops.hackclub.com/api/workshops').then(r => r.json())
   res.json(data)

079024482239dc6e2fe6dee5969ba9f22f49cd2c
faisalsayed10 <faisal.sayed502@gmail.com>
2022-01-24 23:04:58 +0530

    Fix tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    variants: {},
-    backgroundColor: (theme) => ({
-      ...theme("colors"),
-      p_dark: "#22292F",
-    }),
-    extend: {
-      backgroundImage: (theme) => ({
-        main: "url('/bg-main.jpg')",
-        wave: "url('/wave-haikei.svg')",
-        wave2: "url('/wave2.svg')",
-        carbon: "url('/carbon-bg.svg')",
-      }),
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	mode: "jit",
+	content: ["./src/**/*.{html,js,ts,tsx}"],
+	purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		variants: {},
+		backgroundColor: (theme) => ({
+			...theme("colors"),
+			p_dark: "#22292F"
+		}),
+		extend: {
+			backgroundImage: (theme) => ({
+				main: "url('/bg-main.jpg')",
+				wave: "url('/wave-haikei.svg')",
+				wave2: "url('/wave2.svg')",
+				carbon: "url('/carbon-bg.svg')"
+			})
+		}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
 };

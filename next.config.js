98b3a919c9f70dcf005361cfcadfae03b1559332
faisalsayed10 <faisal.sayed502@gmail.com>
2022-02-06 14:32:43 +0530

    Add umami and a redirect

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
+	async redirects() {
+		return [
+			{
+				source: "/firefiles-selfhost",
+				destination: "https://firefiles.vercel.app/docs/self-hosted/02-deploy",
+				permanent: true
+			}
+		];
+	},
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ["@svgr/webpack"]
+		});
 
-    return config;
-  },
+		return config;
+	}
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,31 +4,39 @@ import dynamic from "next/dynamic";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import Head from "next/head";
 
 const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
-  ssr: false,
+	ssr: false
 });
 
 function MyApp({ Component, pageProps }) {
-    useEffect(() => {
-      AOS.init();
-    }, []);
+	useEffect(() => {
+		AOS.init();
+	}, []);
 
-
-  return (
-    <>
-      <AnimatedCursor
-        // @ts-ignore
-        innerSize={10}
-        outerSize={8}
-        color="0, 216, 214"
-        outerAlpha={0.2}
-        innerScale={0.7}
-        outerScale={5}
-      />
-      <Component {...pageProps} />
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<script
+					async
+					defer
+					data-website-id="f492a1fb-d553-4b4d-a09e-a95588092ecc"
+					src="https://umami.fayd.me/umami.js"
+				></script>
+			</Head>
+			<AnimatedCursor
+				// @ts-ignore
+				innerSize={10}
+				outerSize={8}
+				color="0, 216, 214"
+				outerAlpha={0.2}
+				innerScale={0.7}
+				outerScale={5}
+			/>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
